174d067ff5b52e9e352ca97ce6f6d82ed9399d90
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-06-06 22:04:41 +0600

    new work

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiSearch, FiAlignJustify } from "react-icons/fi";
+const Navbar = (props) => {
+  const { onOpenSidenav, brandText } = props;
+
+  return (
+    <nav className="sticky top-3 z-40 flex flex-row flex-wrap items-center justify-between rounded-xl  p-2 backdrop-blur-lg bg-white/5">
+      <div className="ml-[6px]">
+        <div className="h-6 w-[224px] pt-1">
+          <a
+            className="text-sm font-normal text-gray-700 hover:underline "
+            href=" "
+          >
+            Pages
+            <span className="mx-1 text-sm text-gray-700 hover:text-gray-700 ">
+              {" "}
+              /{" "}
+            </span>
+          </a>
+          <Link
+            className="text-sm font-normal capitalize text-gray-700 hover:underline "
+            to="#"
+          >
+            {brandText}
+          </Link>
+        </div>
+        <p className="shrink text-[33px] capitalize text-gray-700 ">
+          <Link to="#" className="font-bold capitalize hover:text-gray-700 ">
+            {brandText}
+          </Link>
+        </p>
+      </div>
+      <span
+        className="flex cursor-pointer text-xl text-gray-600 xl:hidden"
+        onClick={onOpenSidenav}
+      >
+        <FiAlignJustify className="h-5 w-5" />
+      </span>
+      <div className="relative mt-[3px] flex h-[61px] w-[355px] flex-grow items-center justify-around gap-2 rounded-full bg-white px-2 py-2 shadow-xl shadow-[#7090b0]  md:w-[365px] md:flex-grow-0 md:gap-1 xl:w-[365px] xl:gap-2">
+        <div className="flex h-full items-center rounded-full bg-[#F4F7FE] text-gray-700  xl:w-[225px]">
+          <p className="pl-3 pr-2 text-xl">
+            <FiSearch className="h-4 w-4 text-gray-400 " />
+          </p>
+          <input
+            type="text"
+            placeholder="Search..."
+            className="block h-full w-full rounded-full bg-[#F4F7FE] text-sm font-medium text-navy-700 outline-none placeholder:!text-gray-400 sm:w-fit"
+          />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/menu.js
@@ -0,0 +1,105 @@
+import React from "react";
+import {
+    MdHome,
+} from "react-icons/md";
+import { BiSolidCategoryAlt, BiSupport } from "react-icons/bi";
+import { FaCartFlatbed, FaMoneyCheck, FaRocketchat, FaUser } from "react-icons/fa6";
+import { CiShoppingBasket } from "react-icons/ci";
+import { ImLocation } from "react-icons/im";
+import { IoIosRocket } from "react-icons/io";
+import { BsFillHandIndexFill, BsTicketDetailed } from "react-icons/bs";
+const menu = [
+    {
+        name: "Dashboard",
+        layout: "/admin",
+        path: "default",
+        icon: <MdHome className="h-6 w-6" />,
+        navMenu: true,
+    },
+    {
+        name: "Vendor",
+        layout: "/admin",
+        path: "vendor",
+        icon: <FaUser className="h-6 w-6" />,
+        navMenu: true,
+    },
+    {
+        name: "Customer",
+        layout: "/admin",
+        path: "customer",
+    },
+    {
+        name: "Categories",
+        layout: "/admin",
+        path: "categories",
+        navMenu: true,
+        icon: <BiSolidCategoryAlt className="h-6 w-6" />,
+    },
+    {
+        name: "Products",
+        layout: "/admin",
+        path: "products",
+        navMenu: true,
+        icon: <FaCartFlatbed className="h-6 w-6" />,
+    },
+
+    {
+        name: "Order & Review",
+        layout: "/admin",
+        path: "order-review",
+        navMenu: true,
+        icon: <CiShoppingBasket className="h-6 w-6" />,
+    },
+    {
+        name: "Finance",
+        layout: "/admin",
+        path: "finance",
+        navMenu: true,
+        icon: <FaMoneyCheck className="h-6 w-6" />,
+    },
+    {
+        name: "Shipment",
+        layout: "/admin",
+        path: "shipment",
+        navMenu: true,
+        icon: <ImLocation className="h-6 w-6" />,
+    },
+    {
+        name: "Ads Manager",
+        layout: "/admin",
+        path: "ads-manager",
+        navMenu: true,
+        icon: <IoIosRocket className="h-6 w-6" />,
+    },
+
+    {
+        name: "Vouchers",
+        layout: "/admin",
+        path: "vouchers",
+        navMenu: true,
+        icon: <BsTicketDetailed className="h-6 w-6" />,
+    },
+    {
+        name: "Event Manager",
+        layout: "/admin",
+        path: "event-manager",
+        navMenu: true,
+        icon: <BsFillHandIndexFill className="h-6 w-6" />,
+    },
+    {
+        name: "Message Center",
+        layout: "/admin",
+        icon: <FaRocketchat className="h-6 w-6" />,
+        path: "message-center",
+        navMenu: true,
+    },
+    {
+        name: "Support",
+        layout: "/admin",
+        icon: <BiSupport className="h-6 w-6" />,
+        path: "message-center",
+        navMenu: true,
+    },
+
+];
+export default menu;
