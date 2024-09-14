615f8ae319a3425a1edebb40fe502e13ae2990c6
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-09-14 19:04:39 +0600

    up

--- src/components/Dashboard/Finance/ReceivalePayment.jsx
+++ src/components/Dashboard/Finance/ReceivalePayment.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { financeData } from "../../../utils/constant";
+import useUserStore from "../../../store/AuthStore";
+import useOrderStore from "../../../store/OrderStore";
 
 const ReceivalePayment = () => {
-  
+  const { users, fetchAllUser } = useUserStore();
+  const [userOrders, setUserOrders] = useState({});
+
+  const { fetchCustomerOrders } = useOrderStore();
+  useEffect(() => {
+    fetchAllUser();
+  }, [fetchAllUser]);
+
+  useEffect(() => {
+    const loadOrdersForAllUsers = async () => {
+      const ordersByUser = {};
+      for (let user of users) {
+        const orders = await fetchCustomerOrders(user._id);
+        ordersByUser[user._id] = orders;
+      }
+      setUserOrders(ordersByUser);
+    };
+
+    if (users?.length) {
+      loadOrdersForAllUsers();
+    }
+  }, [users, fetchCustomerOrders]);
+
+  const calculateActiveOrders = (orders) => {
+    return orders?.filter((order) => order.status !== "cancelled");
+  };
   const header = [
     "No",
     "Order Id",
@@ -30,19 +57,34 @@ const ReceivalePayment = () => {
           </tr>
         </thead>
         <tbody>
-          {financeData?.map((finance, index) => (
-            <tr key={finance.id} className="border-b border-gray-200">
-              <td className="py-4 px-6">{index}</td>
-              <td className="py-4 px-6">{finance.orderId}</td>
-              <td className="py-4 px-6">{finance.orderCost}</td>
-              <td className="py-4 px-6">{finance.name}</td>
-              <td className="py-4 px-6">{finance.phone}</td>
-              <td className="py-4 px-6">{finance.email}</td>
-              <td className="py-4 px-6">{finance.profit}</td>
-              <td className="py-4 px-6">{finance.brand}</td>
-              <td className="py-4 px-6">{finance.date}</td>
-            </tr>
-          ))}
+          {users?.map((user) => {
+            const orders = userOrders[user._id] || [];
+            const activeOrders = calculateActiveOrders(orders);
+            // const TotalOrderCost = orders.red;
+
+            return (
+              <tbody key={user._id}>
+                <tr className="border-r border-l border-gray-300 border-b">
+                  <td className="text-center text-dark font-medium text-secondary py-5 text-sm bg-transparent border-b border-l border-r border-gray-300">
+                    {user._id}
+                  </td>
+                  <td className="text-center text-dark font-medium text-secondary py-5 px-2 bg-transparent border-b border-r border-gray-300 capitalize">
+                    {user.name}
+                  </td>
+                  <td className="text-center text-dark font-medium text-secondary py-5 px-2 bg-transparent border-b border-r border-gray-300">
+                    {user?.name}
+                  </td>
+                  <td className="text-center text-dark font-medium text-secondary py-5 px-2 cursor-pointer bg-transparent border-b border-r border-gray-300">
+                    {/* {activeOrders} */}
+                  </td>
+                  <td className="text-center text-dark font-medium text-secondary py-5 px-2 cursor-pointer bg-transparent border-b border-r border-gray-300">
+                    {/* {totalOrders} */}
+                  </td>
+                 
+                </tr>
+              </tbody>
+            );
+          })}
         </tbody>
       </table>
     </div>
